5efe14d6191338600cafc403f085f569a818b196
nvnhan <nvnhan.dev@gmail.com>
2022-10-06 13:30:47 +0700

    nvnhan

--- install.bat
+++ install.bat
@@ -5,7 +5,7 @@ echo **********************INSTALL LIB**************************
 call cd ./client
 
 call npm i --legacy-peer-deps
-
+call npx browserslist@latest --update-db
 call cd ../server
 
 call npm i
