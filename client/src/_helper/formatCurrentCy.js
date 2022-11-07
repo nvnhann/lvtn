a449acb3b5b44da20e3d946ae0c8ef08b06c797b
hnghia448448 <71956874+hnghia448448@users.noreply.github.com>
2022-11-07 13:36:45 +0700

    'nvnhna'

--- client/src/_helper/formatCurrentCy.js
+++ client/src/_helper/formatCurrentCy.js
@@ -1,4 +1,5 @@
 export function fCurrency(s) {
+    if (typeof s === "undefined" || typeof s === null || s === '') return ;
     s = s.toString();
     return s.replace(/\B(?=(\d{3})+(?!\d))/g, ',') + ' đ';
 }
