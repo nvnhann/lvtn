ce1766494521941d86ee6a209f007332336d7482
hnghia448448 <71956874+hnghia448448@users.noreply.github.com>
2022-11-08 10:21:11 +0700

    'nvnhan'

--- server/app.js
+++ server/app.js
@@ -152,6 +152,8 @@ require("./routes/hoadon")(app);
 app.get("/", (req, res) => {
     res.send('<a href="/auth/google">Authenticate with Google</a>');
 });
+
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
